fea71d9074462e8a264291ff20635cd31720220c
Megan Morgan <morganml2@cardiff.ac.uk>
2018-04-23 18:58:40 +0100

    Initial Commit

--- emporium/urls.py
+++ emporium/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =  [
-	url(r'^$', views.product_listing, name='product_listing')
-	]
+	url(r'^$', views.product_listing, name='product_listing'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
+        ]
--- emporium/views.py
+++ emporium/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from emporium.forms import PostForm
+
 
 def product_listing(request):
-    return render(request, 'emporium/product_listing.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'emporium/product_listing.html', {'posts':posts})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'emporium/post_edit.html', {'form':form})
+
